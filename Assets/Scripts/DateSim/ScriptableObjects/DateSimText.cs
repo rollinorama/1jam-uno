f06033368d501a2512b13544690ed6da6638ae67
Bruno Rollin <rollinorama@gmail.com>
2020-04-25 22:14:03 -0300

    Add conversation

--- Assets/Scripts/DateSim/ScriptableObjects/DateSimText.cs
+++ Assets/Scripts/DateSim/ScriptableObjects/DateSimText.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SG.DateSim
 {
-    [CreateAssetMenu(fileName = "DateSimState", menuName = "States/DateSimState")]
+    [CreateAssetMenu(fileName = "DateSimText", menuName = "States/DateSimText")]
     public class DateSimText : ScriptableObject
     {
         [TextArea(14, 10)] public  string textMessage;
         public DateSimAnswer[] answers;
-
+        public Image _image;
     }
 
 }
